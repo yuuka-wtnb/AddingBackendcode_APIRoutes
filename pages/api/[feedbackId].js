7b0035caa01cb046409ba799c6bd9daa0dd49ae2
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-07-28 23:56:59 +0900

    Creating & Using Dynamic API Routes

---
+++ pages/api/[feedbackId].js
@@ -0,0 +1,18 @@
+import { buildFeedbackPath, extractFeedback } from "./feedback";
+
+function handler(req, res) {
+  const feedbackId = req.query.feedbackId;
+  const filePath = buildFeedbackPath();
+  //array of all items
+  const feedbackData = extractFeedback(filePath);
+
+  //use .find() to find one single item
+  const selectedFeedback = feedbackData.find(
+    (feedback) => feedback.id === feedbackId
+  );
+
+  //feedbackDataというオブジェクトを、feedbackというキーを持つ形でJSON形式で返す
+  res.status(200).json({ feedback: selectedFeedback });
+}
+
+export default handler;
--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,29 @@
+import { Fragment, useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState();
+
+    //set id as a parameter
+    function loadFeedbackHandler(id){
+        //fetch(`/api/${id}`)でもOK // /api/some-feedback-id
+        fetch('/api/' + id).then(response => response.json()).then(data => {
+            setFeedbackData(data.feedback);
+        });
+
+    }
   return (
+    //コンポーネントでfeedbackDataを使用して、条件付きレンダリングを行う
+    //feedbackDataがセットされている場合、段落タグにfeedbackData.emailを出力して、フィードバックのメールアドレスを表示
+    <Fragment>
+        {feedbackData && <p>{feedbackData.email}</p>}
     <ul>
       {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <li key={item.id}>
+            {item.text}<button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button></li>
       ))}
     </ul>
+    </Fragment>
   );
 }
 export async function getStaticProps() {
